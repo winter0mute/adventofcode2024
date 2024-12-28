e992d61bd5c2571cf5edc6180a8ddb389710b5cc
winter0mute <17337097+winter0mute@users.noreply.github.com>
2024-12-28 23:59:07 +0100

    Add day13

---
+++ days/13/README.md
@@ -0,0 +1,11 @@
+# Day 13
+
+URL: https://adventofcode.com/2024/day/13
+
+## Task
+* _part1_: Get the minimal value of movements that satisfies the prise value ie: do some A and some B movements, add the values together and if those match the prize values then then the combination is valid, the value of this combination is tree times the A buttons pressed and one times the B button pressed added together.
+* _part2_: We don't have the restriction to find a valid movement combination after 100 button presses but the prize should be increased by 10000000000000.
+
+## Solution
+* _part1_: `./day13part1.js input`
+* _part2_: `./day13part2.js input`
---
+++ days/13/day13part2.js
@@ -0,0 +1,44 @@
+#!/usr/bin/env node
+import { EOL } from 'os';
+import { readFileSync } from 'fs';
+
+var fileToProcess = process.argv.slice(2,3);
+
+var data = readFileSync(fileToProcess.toString()).toString().split(EOL);
+
+let loopObject={};
+let clawMachines=[];
+let numberOfTokens=0;
+const prizeBump =10000000000000;
+
+for (let index = 0; index < data.length; index++) {
+    switch (index%4) {
+        case 0:
+            loopObject.ax=Number(data[index].split('+')[1].split(',')[0]);
+            loopObject.ay=Number(data[index].split('+')[2]);
+            break;
+        case 1:
+            loopObject.bx=Number(data[index].split('+')[1].split(',')[0]);
+            loopObject.by=Number(data[index].split('+')[2]);
+            break;
+        case 2:
+            loopObject.px=Number(data[index].split('=')[1].split(',')[0])+prizeBump;
+            loopObject.py=Number(data[index].split('=')[2])+prizeBump;
+            break;
+        default:
+            clawMachines.push(loopObject);
+            loopObject={};
+            break;
+    }
+}
+
+clawMachines.forEach(element => {
+    let ca = (element.px * element.by - element.py * element.bx) / (element.ax * element.by - element.ay * element.bx);
+    let cb = (element.px - element.ax * ca) / element.bx;
+    
+    if (Number.isInteger(ca) && Number.isInteger(cb)) {
+        numberOfTokens+=ca*3+cb;
+    }
+});
+
+console.log(numberOfTokens);
